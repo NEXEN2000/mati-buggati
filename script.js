bba552fe0229f8633865e4319082d581a5864180
Borysus200 <reklama.innovation.group@gmail.com>
2025-04-23 22:02:50 +0200

    added clicker and animations

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Mateusz ma GYATT RIZZ");
+
+const kliker = document.getElementById("kliker");
+const clickSound = new Audio("sounds/nom_c_06-89142.mp3");
+
+kliker.addEventListener("click", () => {
+  clickSound.currentTime = 0;
+  clickSound.play();
+});
